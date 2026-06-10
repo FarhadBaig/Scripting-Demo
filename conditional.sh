84a936d266475219f817e7dd6fa7b5d2c2ea26a4
Farhad Baig <farhadbaig1010@gmail.com>
2026-06-10 11:46:37 +0000

    Added shell scripting files

---
+++ conditional.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+read -p "Enter your name: " name
+if [ "$name" = "Farhad" ]; then
+	echo "Hello Farhad"
+else
+	echo "you are not Farhad"
+fi
---
+++ variable.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+name="Farhad"
+echo "Hello $name"
